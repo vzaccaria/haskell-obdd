a014d3f8c35dfac5c6f317ca318db88cf606acf4
Vittorio Zaccaria <vittorio.zaccaria@gmail.com>
2016-10-10 21:38:31 +0200

    fix: group tests

--- src/OBDD/Melt/Test.hs
+++ src/OBDD/Melt/Test.hs
@@ -7,6 +7,8 @@ import OBDD.Melt.Operator (plotFinal, plotDotWithLabels, evalMop, asMop)
 import OBDD.Data
 import qualified OBDD.Operation as O
 import Test.QuickCheck
+import Test.Tasty
+import Test.Tasty.QuickCheck as QC
 import Data.Bool ((||),(&&))
 
 -- Variables for the test
@@ -35,14 +37,14 @@ test3bool b1 b2 b3 b4 = [ test2bool b1 b2 b3 b4, test1bool b1 b2 b3 b4]
 -- Properties to validate with `quickCheck prop`
 --
 
-test1 :: Bool -> Bool -> Bool 
+test1 :: Bool -> Bool -> Bool
 test1 b1 b2 = evalMop test1mop var == [test1bool b1 b2 False False] where
   var 1 = b1
   var 2 = b2
 
 x `xor` y = (x || y) && (not (x && y))
 
-test2 :: Bool -> Bool -> Bool -> Bool -> Bool 
+test2 :: Bool -> Bool -> Bool -> Bool -> Bool
 test2 b1 b2 b3 b4 = evalMop test2mop var == [test2bool b1 b2 b3 b4] where
   var 1 = b1
   var 2 = b2
@@ -50,8 +52,22 @@ test2 b1 b2 b3 b4 = evalMop test2mop var == [test2bool b1 b2 b3 b4] where
   var 4 = b4
 
 
-test3 b1 b2 b3 b4 = evalMop test3mop var == test3bool b1 b2 b3 b4 where 
+test3 b1 b2 b3 b4 = evalMop test3mop var == test3bool b1 b2 b3 b4 where
   var 1 = b1
   var 2 = b2
   var 3 = b3
   var 4 = b4
+
+
+testGroup1 = testGroup "1-output BDD vs actual TT" [
+                                                  QC.testProperty "a or b" test1,
+                                                  QC.testProperty "a or b and (c xor d)" test2
+                                                  ]
+
+testGroup2 = testGroup "2-output BDD vs actual 2-TT" [
+                                                  QC.testProperty "[a or b, a or b and (c xor d)]" test3
+                                                  ]
+
+tests = testGroup "Tests" [testGroup1, testGroup2]
+
+test = defaultMain tests
